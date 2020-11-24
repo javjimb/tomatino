e91e00ca497a0d37f40c48a59846ed434aa97e20
Javier <javjimb@gmail.com>
2020-11-24 15:20:21 +0100

    Add dependencies, assets and basic timer functionality

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,85 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <tomato timer-mode="huhu" ></tomato>
+
+    <div class="clock">{{minutes}}:{{seconds}}</div>
+
+    <div class="controls">
+      <div v-if="!isTimerRunning" @click="startTimer">Play</div>
+      <div v-if="isTimerRunning" @click="stopTimer">Stop</div>
+    </div>
+
+    <br />
+    Pomodoro | Short Break | Long Break
+    <br />
+    <br />
+
+    Pomodoros completed: 0
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tomato from "@/components/Tomato";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Tomato
+  },
+  data: () => ({
+    sessionLength: 25 * 60,
+    elapsedSeconds: 0,
+    remainingSeconds: 0,
+    isTimerRunning: false,
+    timer: null
+  }),
+  methods: {
+    startTimer() {
+      this.remainingSeconds = this.sessionLength
+      this.timer = setInterval(() => this.timerTick(), 1000)
+      this.isTimerRunning = true
+      //new Notification('Pomodoro Timer', { body: 'New pomodoro has started', icon: notificationIcon});
+      //new Notification('Pomodoro Timer', { body: 'New pomodoro has started'})
+      /*
+      var audio = new Audio('http://javierjimenez.net/media/timer_start.wav');
+      audio.play();
+      this.todo.started = true;
+      this.todo.running = true;
+      this.$emit('timer-start', this.todo.task_id);
+      this.$forceUpdate();
+       */
+    },
+    stopTimer() {
+      clearInterval(this.timer)
+      this.timer = null
+      this.remainingSeconds = 0
+      this.isTimerRunning = false
+      /*
+      this.todo.running = false;
+      this.$emit('timer-stop', this.todo.task_id)
+      this.$forceUpdate();
+       */
+    },
+    padTime: function(time) {
+      return (time < 10 ? '0' : '') + time
+    },
+    timerTick: function() {
+      this.remainingSeconds--
+      if (this.remainingSeconds === 0) {
+        this.stopTimer()
+      }
+    }
+  },
+  computed: {
+    minutes: function() {
+      const minutes = Math.floor((this.remainingSeconds % 3600) / 60)
+      return this.padTime(minutes)
+    },
+    seconds: function() {
+      const seconds = Math.floor((this.remainingSeconds % 3600) % 60);
+      return this.padTime(seconds);
+    }
   }
 }
 </script>
@@ -22,7 +90,16 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  background-color: #E6203C;
+  color: black;
+}
+.clock {
+  font-size: 6rem;
+}
+.controls {
+  font-size: 2rem;
 }
 </style>
---
+++ src/components/Tomato.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img :src="tomate" />
+  </div>
+</template>
+
+<script>
+import Tomate from '@/assets/images/tomate.png'
+
+export default {
+  name: 'Tomato',
+  props: {
+    timerMode: {
+      type: String,
+      required: true
+    }
+  },
+  data: () => ({
+    tomate: Tomate
+  })
+}
+</script>
