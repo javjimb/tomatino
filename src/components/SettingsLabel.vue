90dda3fbe7075440917da31a9f7489ff1c20d844
Javier <javjimb@gmail.com>
2020-11-27 11:59:50 +0100

    Create SettingsLabel component

---
+++ src/components/SettingsLabel.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3 class="settings-label">{{label}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SettingsLabel',
+  props: {
+    label: {
+      type: String,
+      default: 'Settings label'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.settings-label {
+  padding-left: 15px;
+  color: #424242;
+}
+</style>
