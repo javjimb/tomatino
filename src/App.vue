2470782f61c7da916f88c6ebb477e7cef897dac0
Javier <javjimb@gmail.com>
2020-12-03 11:22:46 +0100

    Separate footer in its own component

--- src/App.vue
+++ src/App.vue
@@ -7,70 +7,19 @@
       </v-container>
     </v-main>
 
-    <v-footer
-        app
-        padless
-    >
-      <v-divider></v-divider>
-      <v-card
-          flat
-          tile
-          width="100%"
-          class="footer text-center"
-      >
-        <v-card-text class="pa-0">
-          <v-tooltip top>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                  v-show="$route.name !== 'settings'"
-                  class="mx-4"
-                  icon
-                  to="/settings"
-                  v-bind="attrs"
-                  v-on="on"
-              >
-                <v-icon size="24px">
-                  mdi-cog
-                </v-icon>
-              </v-btn>
-            </template>
-            <span>Settings</span>
-          </v-tooltip>
-
-          <v-tooltip top>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                  v-show="$route.name !== 'timer'"
-                  class="mx-4"
-                  icon
-                  to="/"
-                  v-bind="attrs"
-                  v-on="on"
-              >
-                <v-icon size="24px">
-                  mdi-clock
-                </v-icon>
-              </v-btn>
-            </template>
-            <span>Timer</span>
-          </v-tooltip>
-
-        </v-card-text>
-
-
-
-      </v-card>
-    </v-footer>
+    <Footer></Footer>
 
   </v-app>
 </template>
 
 <script>
+import Footer from '@/components/Footer'
 
 export default {
   name: 'App',
 
   components: {
+    Footer
   },
 
   data: () => ({
@@ -83,8 +32,5 @@ export default {
 <style>
 .v-application {
   background-color: #E6203C !important;
-}
-.footer {
-  background-color: #cf1731 !important;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-footer
+      app
+      padless
+  >
+    <v-divider></v-divider>
+    <v-card
+        flat
+        tile
+        width="100%"
+        class="footer text-center"
+    >
+      <v-card-text class="pa-0">
+        <v-tooltip top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+                v-show="$route.name !== 'settings'"
+                class="mx-4"
+                icon
+                to="/settings"
+                v-bind="attrs"
+                v-on="on"
+            >
+              <v-icon size="24px">
+                mdi-cog
+              </v-icon>
+            </v-btn>
+          </template>
+          <span>Settings</span>
+        </v-tooltip>
+
+        <v-tooltip top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+                v-show="$route.name !== 'timer'"
+                class="mx-4"
+                icon
+                to="/"
+                v-bind="attrs"
+                v-on="on"
+            >
+              <v-icon size="24px">
+                mdi-clock
+              </v-icon>
+            </v-btn>
+          </template>
+          <span>Timer</span>
+        </v-tooltip>
+      </v-card-text>
+
+    </v-card>
+  </v-footer>
+</template>
+
+<style scoped>
+.footer {
+  background-color: #cf1731 !important;
+}
+</style>
